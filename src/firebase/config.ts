c7c013149516101c1198470468267680343ad9b7
Tamer Özkara <tamerozkara02@gmail.com>
2026-01-15 08:17:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6865482988-7a434",
+  "appId": "1:917559459242:web:6b88a8582e87c08f35bfd3",
+  "apiKey": "AIzaSyAeGUOmCkbQJhp8ouUImIYIbcJmeXq1_dQ",
+  "authDomain": "studio-6865482988-7a434.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "917559459242"
+};
